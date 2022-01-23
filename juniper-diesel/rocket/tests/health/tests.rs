e1e3995c28edd0409f4b2ca4b57b81d926c4fd63
azzamsa <me@azzamsa.com>
2022-01-23 12:04:06 +0700

    test(juniper-diesel-rocket): use cynic to build GraphQL query
    Using plain text is painful for complex queries.

--- juniper-diesel/rocket/tests/health/tests.rs
+++ juniper-diesel/rocket/tests/health/tests.rs
@@ -1,22 +1,35 @@
+use cynic::QueryBuilder;
 use rocket::http::{ContentType, Status};
+use serde::Deserialize;
+use zoo::health::schema::Health;
+
+use super::graphql::queries::HealthQuery;
+
+#[derive(Debug, Deserialize)]
+pub struct Response {
+    pub data: HealthWrapper,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct HealthWrapper {
+    pub health: Health,
+}
 
 #[test]
 fn test_health() {
     use rocket::local::blocking::Client;
-    use zoo::health;
 
     let client = Client::tracked(zoo::rocket()).unwrap();
+    let query = HealthQuery::build(());
+
     let resp = client
         .post("/graphql")
         .header(ContentType::JSON)
-        .body(r#"{"query": "{ health { status } }" }"#)
+        .json(&query)
         .dispatch();
 
     assert_eq!(resp.status(), Status::Ok);
 
-    let body: serde_json::Value = serde_json::from_slice(&resp.into_bytes().unwrap()).unwrap();
-    let health_value = &body["data"]["health"];
-    let health: health::schema::Health = serde_json::from_value(health_value.clone()).unwrap();
-
-    assert_eq!(health.status, "running");
+    let response = resp.into_json::<Response>().unwrap();
+    assert_eq!(response.data.health.status, "running");
 }
